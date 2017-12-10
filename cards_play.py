6883cd824119d7892f0e5decaa63001d3bc87fdc
samghadri <sam.ghadri@gmail.com>
2017-12-10 20:31:17 +0000

    sortingCards

--- cards_play.py
+++ cards_play.py
@@ -1,17 +1,16 @@
 import collections
-from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
-class FrenchDeck:
 
-    ranks = [str(n) for n in range(2,11)] + list('JKQA')
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JKQA')
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for rank in self.ranks
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
 
-                                        for suit in self.suits]
     def __len__(self):
         return len(self._cards)
 
@@ -22,5 +21,14 @@ class FrenchDeck:
 hand = FrenchDeck()
 
 print(len(hand))
-print(hand[51])
-print(choice(hand))
+
+print(hand[12::13])
+
+suits_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spade_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suits_values) + suits_values[card.suit]
+
+for card in sorted(hand, key=spade_high):
+    print(card)
